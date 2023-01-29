4cafc7fe74fcd49faed786d8ce07ff110a2b51a0
Vityagi <Vikrant.tyg1995@gmail.com>
2023-01-29 13:05:40 +0530

    Added solution to day 2 ex 1

---
+++ Day-2/data_types.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+num_str = str(two_digit_number)
+first_digit = int(num_str[0])
+second_digit = int(num_str[1])
+
+print(first_digit + second_digit)
